dd40b7096f519d71009dc8de21d778ef87153e95
GE28 <44209041+GE28@users.noreply.github.com>
2022-12-26 19:27:06 -0300

    Adiciona arquivos do servidor
    Uso do Nodemon para monitorar arquivos
    Uso do morgan para realizar o log de requisições

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import morgan from 'morgan';
+const app = express();
+
+app.use(morgan('dev'));
+app.get('/', (req, res) => {
+  return res.send('Hello world');
+})
+
+app.listen(3000);
