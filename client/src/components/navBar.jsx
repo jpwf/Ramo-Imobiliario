0ce9c5e7b0e3f698b40d765fad73e391cec171df
jpwf <joaopedroweydt@gmail.com>
2022-12-27 00:29:29 -0300

    Done, waiting for group review, before the oficial project review

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import logo from '../../public/logo.png'
 // import {Link} from 'react-router-dom'
 function Navbar() {
   return (
     <div>
       
       <div className=" flex flex-row justify-center items-center text-base mt-12  ">
-        <h1 className="  mt-18 mb-9 ml-16">Logo</h1>
-        <div className="mr-56 ml-48 ">
-          <button className="mx-16"> Início </button>
-          <button className="mx-16"> Buscar imóveis</button>
-          <button className="mx-16"> Anunciar </button>
+        <img className='mt-4 'src={logo}/>
+        <div className="mr-56 ml-48">
+          <button className="mx-8"> Início </button>
+          <button className="mx-8"> Buscar imóveis</button>
+          <button className="mx-8"> Anunciar </button>
         </div>
         <div className="mr-8 ">
           <button className="mr-8"> Entrar </button>
