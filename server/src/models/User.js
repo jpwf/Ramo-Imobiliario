131300d0baaad577867a8bc8612595052b0234b3
devAna <caldas.ana9@gmail.com>
2022-12-26 22:37:04 -0300

    Alteracao no Model User

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema
 const ObjectId = Schema.ObjectId
 
 const UserSchema = new Schema({
     id: ObjectId,
     email: String,
-    senha: String, 
+    hashSenha: String, 
     nomeCompleto: String,
     telefone: Number, 
-    cpf: String,
+    cpf: String
 })
 
 const UserModel = mongoose.model('users', UserSchema)
-module.exports = UserSchema
+
+export default UserModel
+
+
 
