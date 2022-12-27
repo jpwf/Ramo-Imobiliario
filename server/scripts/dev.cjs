c4f715b0495c454181089638f184f4fca35fdb7d
GE28 <44209041+GE28@users.noreply.github.com>
2022-12-27 19:35:20 -0300

    Substitui uso do spawn por import

--- server/scripts/dev.cjs
+++ server/scripts/dev.cjs
@@ -6,15 +6,11 @@ function importDotEnv(envPath) {
     if (fs.existsSync(envPath)) {
         dotenv.config({ path: envPath });
     } else {
-        console.log('\x1b[31m%s\x1b[0m', 'File .env not found. Try to copy .env.example to .env and fill it with your own data.');
-        return 1;
+        console.log('\x1b[31m%s\x1b[0m', 'Try to copy .env.example to .env and fill it with your own data.');
+        throw new Error('.env file not found');
     }
 }
 
-function startServer(indexPath) {
-    require('child_process').spawn(indexPath, { stdio: 'inherit', shell: true });
-}
-
 if (!importDotEnv(path.join(__dirname, '..', '.env'))) {
-    startServer(`nodemon ${path.join(__dirname, '..', 'src', 'index.js')}`);
+    import('../src/index.js');
 }
