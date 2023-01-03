bdccbe2269e933fdb82f782d0b186a4e29bd371e
jpwf <joaopedroweydt@gmail.com>
2023-01-03 10:49:38 -0300

    card component created

--- client/src/pages/Busca.jsx
+++ client/src/pages/Busca.jsx
@@ -1,23 +1,44 @@
-
+import Card from '../components/Card'
+import cardImage from '../assets/cardImage.png'
 function Busca() {
     return(
         <div>
-            <div className='justify-between'>
-                <div className='flex flex-col'>
-                    <h3>Bairro</h3>
-                    <select></select>
-                </div>
-                <div className='flex flex-col'>
-                    <h3>Número de quartos</h3>
-                <select></select>
+            <div className='flex justify-between my-4'>
+                <div className='flex px-3'>
+                    <div className='flex flex-col mx-12'>
+                        <h3>Bairro</h3>
+                        <select>
+                             <option></option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className='flex flex-col ml-12'>
+                        <h3>Número de quartos</h3>
+                        <select>
+                            <option></option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
                 </div>
                 
-                
+                <select className='mr-8'>
+                    <option disabled>Selecione</option>
+                </select>    
             </div>
-            <select></select>
+            
+            <Card title='Jorge aragão' img={cardImage} bairro='Praia de Botafogo' rooms='4 Quartos' price='R$ 100000' />
             
             
         </div>
+        
     )
 }
 
