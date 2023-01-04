767fdcb4acab493fec48cf48964ce9ad55169cff
GE28 <44209041+GE28@users.noreply.github.com>
2023-01-04 01:31:15 -0300

    Permite criação e consulta de apartamentos

--- server/src/index.js
+++ server/src/index.js
@@ -3,16 +3,15 @@ import morgan from 'morgan';
 import MongoDB from './database/MongoDB.js';
 
 import userRoutes from './routes/userRoutes.js';
+import apartmentRoutes from './routes/apartmentRoutes.js';
 
 const app = express();
 
 app.use(process.env.enviroment === 'development' ? morgan('dev') : morgan('combined'));
 app.use(express.json());
-app.mongoose = new MongoDB();
+app.mongo = new MongoDB();
 
 app.use('/user', userRoutes);
-app.use('/login', userRoutes);
-app.use('/apartment', userRoutes);
-
+app.use('/apartment', apartmentRoutes);
 
 app.listen(process.env.port);
