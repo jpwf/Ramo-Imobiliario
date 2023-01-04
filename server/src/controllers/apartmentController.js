c8a20e2508e9f4ef89b132eb0ecb1f462b93975b
GE28 <44209041+GE28@users.noreply.github.com>
2023-01-04 17:10:03 -0300

    .

--- server/src/controllers/apartmentController.js
+++ server/src/controllers/apartmentController.js
@@ -2,19 +2,24 @@ import ApartmentModel from '../models/Apartment.js';
 
 export default class ApartmentController {
     static async getSome(req, res) {
-        const { numberOfBedrooms, district, page, limit, sortBy } = req.query;
+        const { page, limit, sortBy } = req.query;
         
         try {
-            const apartments = await ApartmentModel.find({
-                numberOfBedrooms,
-                'address.district': district
-            })
+            const apartments = await ApartmentModel.find(buildQuery(req.query))
                 .limit(limit)
                 .skip((page - 1) * limit)
                 .sort(sortBy);
             res.status(200).json(apartments);
         } catch (error) {
             res.status(500).json({ message: error.message });
+        }
+
+        function buildQuery(query) {
+            const { numberOfBedrooms, district } = query;
+            return {
+                ...(numberOfBedrooms && { numberOfBedrooms }),
+                'address.district': district
+            }
         }
     }
     
