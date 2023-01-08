9445216b056de114089c762432a04f6b54263983
jpwf <joaopedroweydt@gmail.com>
2023-01-08 12:23:01 -0300

    footer started

--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -1,9 +1,18 @@
-
+import bgImage from '../assets/backgroundImg.png'
 
 function Inicio() {
     return(
-        <div>
-            <h1>Página inicial</h1>
+        <div className='bg-src-{bgImage}'>
+            <footer className='flex justify-between bg-blue-300 px-24'>
+                (21)99999-9999
+                <div className='flex flex-col'>
+                    <h2>Endereço</h2>
+                    <p>Rua Gen. Canabarro, 485 - Maracanã. Rio de Janeiro - RJ </p>
+                    <p>(21)4002-8922</p>
+                </div>
+                <p>Nossas redes sociais</p>
+                
+            </footer>
         </div>
     )
 }
