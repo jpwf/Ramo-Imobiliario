2329bef7db8ae46679262fc0130ac96a5d75fbe7
jpwf <joaopedroweydt@gmail.com>
2023-01-09 11:56:08 -0300

    Background image placed, footer created, need the icons from phosphor-react

--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -1,11 +1,19 @@
-import bgImage from '../assets/backgroundImg.png'
+// import backImg from '../assets/backgroundImg.png'
 
 function Inicio() {
     return(
-        <div className='bg-src-{bgImage}'>
-            <footer className='flex justify-between bg-blue-300 px-24'>
+        <div>
+            <div className=' bg-home  bg-cover w-[1440px] h-[708px]'>
+                <div className='text-3xl font-bold text-white pt-56 ml-24 '>
+                    <p>Encontre um lar na</p>
+                    <p>cidade do </p>
+                    <p>Rio de Janeiro</p>
+                </div>
+                
+            </div>
+            <footer className='flex justify-between items-center text-white  bg-blue-400 px-24'>
                 (21)99999-9999
-                <div className='flex flex-col'>
+                <div className='flex flex-col  items-center'>
                     <h2>Endereço</h2>
                     <p>Rua Gen. Canabarro, 485 - Maracanã. Rio de Janeiro - RJ </p>
                     <p>(21)4002-8922</p>
