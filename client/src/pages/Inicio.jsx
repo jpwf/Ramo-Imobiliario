5852ccd950d6423654966ac92fd854bc0acdb820
jpwf <joaopedroweydt@gmail.com>
2023-01-11 14:02:12 -0300

    Done

--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -5,49 +5,49 @@ import SelectComponent from '../components/SelectComponent';
 function Inicio() {
     return(
         <div>
-            <div className=' bg-home  bg-cover w-[1440px] h-[708px]'>
+            <div className=' bg-home  bg-cover w-full h-full'>
                 <div className='text-3xl font-bold text-white pt-56 ml-24 '>
                     <p>Encontre um lar na</p>
                     <p>cidade do </p>
                     <p>Rio de Janeiro</p>
                 </div>
-                <div className='flex bg-slate-100 w-{800px} h-{113px} mb-64 '>
-                    <div className='flex flex-col'>
-                        <div className='flex'>
+                <div className='flex gap-4 bg-white opacity-75 rounded w-[800px] h-[113px] mb-[173px] mt-20 ml-24 '>
+                    <div className='flex flex-col items-center mt-5 ml-6'>
+                        <div className='flex items-center mb-[10px] gap-1'>
                             <MapPin size={16} color="#050505" />
                             Cidade
                         </div>
                         
-                        <SelectComponent label='Rio de Janeiro' className='w-{57px} h-{36px}'/>
+                        <SelectComponent label='Rio de Janeiro' className='w-[57px] h-[36px]'/>
                     </div>
-                    <div className='flex flex-col'>
-                        <div className='flex'>
-                            <NavigationArrow size={16} color="#050505" />
+                    <div className='flex flex-col mt-5 mx-12 gap-1 items-center'>
+                        <div className='flex items-center mb-[10px] '>
+                            <NavigationArrow size={16}  color="#050505" />
                             Bairro
                         </div>
                         
-                        <SelectComponent label='Selecione' className='w-{57px} h-{36px}'/>
+                        <SelectComponent label='Selecione' className='w-[57px] h-[36px]'/>
                     </div>
-                    <div className='flex flex-col'>
-                        <div className='flex'>
-                            <Door size={16} color="#050505" />
+                    <div className='flex flex-col  mt-5'>
+                        <div className='flex items-center mb-[10px] gap-1'>
+                            <Door size={16}  color="#050505" />
                             Número de quartos
                         </div>
-                        
-                        <SelectComponent label='Selecione' className='w-{57px} h-{36px}'/>
-                        <button className='bg-blue-400 flex w-{100px} h-{52px}'>
-                            <MagnifyingGlass size={16} color="#f6eaea" />
+                            <SelectComponent label='Selecione' className='w-[57px] h-[36px]'/>
+                        </div>
+                        <button className='bg-blue-500 flex justify-center items-center gap-{10px} rounded px-4 py-3 w-[100px] h-[52px] ml-10 mt-8'>
+                            <MagnifyingGlass size={24} color="#FFFFFF" />
                         </button>
-                    </div>
+                    
 
                 
             </div>
-            <footer className='flex w-{1440px} h-{192px} justify-between items-center text-white  bg-blue-400 px-24'>
+            <footer className='flex w-full h-[96px] justify-between items-center text-white  bg-[#3E6EB0] px-24'>
                 <div className='flex'>
                     <Phone size={20} className='mt-1' color="#eeeeec"/>
                     (21)99999-9999
                 </div>
-                <div className='flex flex-col  items-center'>
+                <div className='flex flex-col items-center  '>
                     <h2>Endereço</h2>
                     <p>Rua Gen. Canabarro, 485 - Maracanã. Rio de Janeiro - RJ </p>
                     <p>(21)4002-8922</p>
