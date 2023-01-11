15ffaa6daf3e64af0c569d80d96867e21917dc33
GE28 <44209041+GE28@users.noreply.github.com>
2023-01-10 21:14:08 -0300

    Adiciona Helmet

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 import MongoDB from './database/MongoDB.js';
-import cors from 'cors'
 
 import userRoutes from './routes/userRoutes.js';
 import apartmentRoutes from './routes/apartmentRoutes.js';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(helmet());
 
 app.use(process.env.enviroment === 'development' ? morgan('dev') : morgan('combined'));
 app.use(express.json());
