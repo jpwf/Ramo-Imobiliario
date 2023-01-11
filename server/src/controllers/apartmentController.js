39365e7e7e5e05109178df59fcb5b7cce8469b90
devAna <caldas.ana9@gmail.com>
2023-01-10 22:45:14 -0300

    alteracao no find para  nao exibir __v e id

--- server/src/controllers/apartmentController.js
+++ server/src/controllers/apartmentController.js
@@ -5,7 +5,7 @@ export default class ApartmentController {
         const { page, limit, sortBy } = req.query;
         
         try {
-            const apartments = await ApartmentModel.find(buildQuery(req.query))
+            const apartments = await ApartmentModel.find(buildQuery(req.query), {'address._id': 0, '__v': 0})
                 .limit(limit)
                 .skip((page - 1) * limit)
                 .sort({"_id": sortBy === 'newer' ? -1 : 1});
@@ -25,7 +25,7 @@ export default class ApartmentController {
     
     static async show(req, res) {
         try {
-            const apartment = await ApartmentModel.findById(req.params.id).select('-__v');
+            const apartment = await ApartmentModel.findById(req.params.id, {'address._id': 0, '__v': 0});
             res.status(200).json(apartment);
         } catch (error) {
             res.status(500).json({ message: error.message });
