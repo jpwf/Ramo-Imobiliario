e0ceca88522b72c1257360e0e66cc6c51a16b8b9
devAna <caldas.ana9@gmail.com>
2023-01-12 10:04:28 -0300

    Alteracoes na classificacao de busca dos apartamentos

--- server/src/controllers/apartmentController.js
+++ server/src/controllers/apartmentController.js
@@ -1,18 +1,29 @@
 import ApartmentModel from '../models/Apartment.js';
+//import UserController from './userController.js';
 
 export default class ApartmentController {
     static async getSome(req, res) {
         const { page, limit, sortBy } = req.query;
+        var value
+
+        if (sortBy === 'cheaper')
+            value = 1
+
+        if (sortBy === 'expensive')
+            value = -1
+
+        console.log(value)
         
         try {
             const apartments = await ApartmentModel.find(buildQuery(req.query), {'address._id': 0, '__v': 0})
                 .limit(limit)
                 .skip((page - 1) * limit)
-                .sort({"_id": sortBy === 'newer' ? -1 : 1});
+                .sort(sortBy === 'cheaper' || sortBy === 'expensive' ? {price: value} : {_id: -1})
             res.status(200).json(apartments);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
+
 
         function buildQuery(query) {
             const { numberOfBedrooms, district } = query;
--- server/src/routes/apartmentRoutes.js
+++ server/src/routes/apartmentRoutes.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import Joi from 'joi';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import apartmentController from '../controllers/apartmentController.js';
-import multer from 'multer';
-import { storage, fileFilter } from '../middlewares/multerConfig.js';
+import multer, { MulterError } from 'multer';
+import { storage, fileFilter, uploadAuth } from '../middlewares/multerConfig.js';
+//import UserController from '../controllers/userController';
 
 const router = Router();
 
 const upload = multer({ 
+    uploadAuth: uploadAuth,
     storage: storage,
     limits: {
         fileSize: 1024 * 1024 * 5
@@ -37,20 +39,22 @@ router.post('/publish', authMiddleware, (req, res, next) => {
 }, apartmentController.create);
 
 
-router.post('/upload', upload.single('image'), (req, res, next) =>{
+router.post('/upload/:id', upload.single('image'), (req, res, next) =>{
+
     if (req.file){
         console.log(req.file)
-        return res.json(req.file.filename)
+        return res.status(200).json({messege: 'Successfully uploaded'})
+        //return res.json(req.file.filename)
     }
 
-    return res.status(400).json({error: 'Por favor, insira um formato de imagem válido!'})
+    return res.status(406).json({error: 'Uploaded file is not a valid image'})
 })
 
 router.get('/search', (req, res, next) => {
     const schema = Joi.object({
         numberOfBedrooms: Joi.number().min(1).max(20),
         district: Joi.string().min(3).max(20).required(),
-        sortBy: Joi.string().valid('newer', 'publication_order').default('publication_order'),
+        sortBy: Joi.string().valid('newer', 'cheaper', 'expensive').default('newer'),
         page: Joi.number().min(1).default(1),
         limit: Joi.number().min(1).max(100).default(20)
     });
