a5e15d462c9ea45c9bebfee2fe38a878369372a2
devAna <caldas.ana9@gmail.com>
2023-01-18 19:20:10 -0300

    Pequena alteração no fileController

--- server/src/controllers/fileController.js
+++ server/src/controllers/fileController.js
@@ -2,14 +2,6 @@ import { MulterError } from 'multer';
 import ApartmentModel from '../models/Apartment.js'
 
 export default class FileController {
-    /*static async checkUpload(res, err){
-        if (err instanceof MulterError){
-            return res.status(406).json({ error: err.message })
-        } else if (err){
-            return res.status(406).json({ error: err.message })
-        }
-    }*/
-    
     static async create(req, res) { 
         const apartment = await ApartmentModel.findById(req.params.id);
 
